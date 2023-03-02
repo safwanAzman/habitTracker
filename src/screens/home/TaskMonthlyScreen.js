f7e0202cac90cb71f8f710c3425373e74ca44769
safwan <safwanazman017@gmail.com>
2023-03-02 10:21:41 +0800

    update

---
+++ src/screens/home/TaskMonthlyScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import BtnBack from '../../components/btn/BtnBack';
+import Container from '../../components/container';
+
+
+export default function TaskMonthlyScreen() {
+    return (
+        <>
+            <BtnBack/>
+            <Container>
+                <Text>
+                    TaskMonthlyScreen   
+                </Text>
+            </Container>
+        </>
+    )
+}
