84ef73fb7b6da5678839f528983d9e6f1cd7ca17
safwan <safwanazman017@gmail.com>
2023-03-02 11:38:45 +0800

    update

---
+++ src/screens/home/TaskWeeklyScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import BtnBack from '../../components/btn/BtnBack';
+import Container from '../../components/container';
+
+
+export default function TaskWeeklyScreen() {
+    return (
+        <>
+            <BtnBack/>
+            <Container>
+                <Text>
+                    TaskWeeklyScreen   
+                </Text>
+            </Container>
+        </>
+    )
+}
