ae26f0ecadf6f503ba7932c2c7b452f4acd34441
safwan <safwanazman017@gmail.com>
2023-03-02 10:21:05 +0800

    update

---
+++ src/screens/home/TaskCompleteScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import BtnBack from '../../components/btn/BtnBack';
+import Container from '../../components/container';
+
+
+export default function TaskCompleteScreen() {
+    return (
+        <>
+            <BtnBack/>
+            <Container>
+                <Text>
+                    TaskCompleteScreen   
+                </Text>
+            </Container>
+        </>
+    )
+}
