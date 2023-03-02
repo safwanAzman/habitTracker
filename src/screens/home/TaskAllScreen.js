4f0b3a7ae5379e8716ee840dae31bdcde908a9dd
safwan <safwanazman017@gmail.com>
2023-03-02 10:20:55 +0800

    update

---
+++ src/screens/home/TaskAllScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import BtnBack from '../../components/btn/BtnBack';
+import Container from '../../components/container';
+
+
+export default function TaskAllScreen() {
+    return (
+        <>
+            <BtnBack/>
+            <Container>
+                <Text>
+                    TaskAllScreen   
+                </Text>
+            </Container>
+        </>
+    )
+}
